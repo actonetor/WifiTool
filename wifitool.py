497faa5b7e740ed2db94f4c6acb1e7b5bc6311af
actonetor <87834920+actonetor@users.noreply.github.com>
2021-10-23 19:17:40 +0200

    Update wifitool.py

--- wifitool.py
+++ wifitool.py
@@ -25,22 +25,21 @@ print("********************************************\n\n\n")
 print("\u001b[31mINSTALLATION DES REQUIREMENT\u001b[0m\n") 
 def loading(count):
     all_progress = [0] * count
-    sys.stdout.write("\n" * count) # Make sure we have space to draw the bars
+    sys.stdout.write("\n" * count)
     while any(x < 100 for x in all_progress):
         time.sleep(0.01)
-        # Randomly increment one of our progress values
         unfinished = [(i, v) for (i, v) in enumerate(all_progress) if v < 100]
         index, _ = random.choice(unfinished)
         all_progress[index] += 1
         
-        # Draw the progress bars
-        sys.stdout.write(u"\u001b[1000D") # Move left
-        sys.stdout.write(u"\u001b[" + str(count) + "A") # Move up
+        sys.stdout.write(u"\u001b[1000D")
+        sys.stdout.write(u"\u001b[" + str(count) + "A")
         for progress in all_progress: 
             width = progress / 4
-            print "[" + "#" * width + " " * (25 - width) + "]"
+            print ("[" + "#" * width + " " * (25 - width) + "]")
         
-loading()os.system("clear")
+loading()
+os.system("clear")
 
 
 #présentation
