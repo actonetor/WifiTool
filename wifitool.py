2cfb97e29547fa73fa1b02a4c55417f3ddd77730
actonetor <87834920+actonetor@users.noreply.github.com>
2021-10-23 19:38:01 +0200

    Update wifitool.py

--- wifitool.py
+++ wifitool.py
@@ -23,22 +23,6 @@ print("* \u001b[34mINSTAGRAM:Lefrentoune\u001b[0m                    *")
 print("*                                          *") 
 print("********************************************\n\n\n") 
 print("\u001b[31mINSTALLATION DES REQUIREMENT\u001b[0m\n") 
-def loading(count):
-    all_progress = [0] * count
-    sys.stdout.write("\n" * count)
-    while any(x < 100 for x in all_progress):
-        time.sleep(0.01)
-        unfinished = [(i, v) for (i, v) in enumerate(all_progress) if v < 100]
-        index, _ = random.choice(unfinished)
-        all_progress[index] += 1
-        
-        sys.stdout.write(u"\u001b[1000D")
-        sys.stdout.write(u"\u001b[" + str(count) + "A")
-        for progress in all_progress: 
-            width = progress / 4
-            print ("[" + "#" * width + " " * (25 - width) + "]")
-        
-loading()
 os.system("clear")
 
 
