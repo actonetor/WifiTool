ed153f8be7374342c221e71ae3b97703e9afe12b
actonetor <87834920+actonetor@users.noreply.github.com>
2021-10-23 19:14:56 +0200

    Update wifitool.py

--- wifitool.py
+++ wifitool.py
@@ -22,21 +22,25 @@ print("* \u001b[34mINSTAGRAM:Lefrentoune\u001b[0m                    *")
 print("*                                          *") 
 print("********************************************\n\n\n") 
 print("\u001b[31mINSTALLATION DES REQUIREMENT\u001b[0m\n") 
-print("[ ] %0 ") 
-time.sleep(2) 
-os.system("apt autoremove ") 
-print("[======= ] %25 ") 
-time.sleep(2) 
-os.system("apt autoclean") 
-print("[============== ] %50 ") 
-time.sleep(2) 
-os.system("apt-get aircrack-ng") 
-print("[===================== ] %75 ") 
-time.sleep(2) 
-os.system("apt-get install fern-wifi-cracker") 
-print("[===========================] %100 ") 
-time.sleep(3) 
-os.system("clear")
+ import time, sys, random
+def loading(count):
+    all_progress = [0] * count
+    sys.stdout.write("\n" * count) # Make sure we have space to draw the bars
+    while any(x < 100 for x in all_progress):
+        time.sleep(0.01)
+        # Randomly increment one of our progress values
+        unfinished = [(i, v) for (i, v) in enumerate(all_progress) if v < 100]
+        index, _ = random.choice(unfinished)
+        all_progress[index] += 1
+        
+        # Draw the progress bars
+        sys.stdout.write(u"\u001b[1000D") # Move left
+        sys.stdout.write(u"\u001b[" + str(count) + "A") # Move up
+        for progress in all_progress: 
+            width = progress / 4
+            print "[" + "#" * width + " " * (25 - width) + "]"
+        
+loading()os.system("clear")
 
 
 #présentation
